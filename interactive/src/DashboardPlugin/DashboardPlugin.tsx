2d2993f15491eadc7be3d3196d5ec7d1fc6eed55
mikebender <mikebender@deephaven.io>
2022-04-29 09:51:53 -0400

    WIP add Inputs panel
    Inputs should show up now...

--- interactive/src/DashboardPlugin/DashboardPlugin.tsx
+++ interactive/src/DashboardPlugin/DashboardPlugin.tsx
@@ -3,13 +3,66 @@ import shortid from 'shortid';
 import {
   DashboardPluginComponentProps,
   LayoutUtils,
+  PanelEvent,
   useListener,
 } from '@deephaven/dashboard';
+import { GoldenLayout } from '@deephaven/golden-layout';
 import { VariableDefinition } from '@deephaven/jsapi-shim';
 import Log from '@deephaven/log';
-import ObjectPanel from './ObjectPanel';
+import InputPanel, { JsWidgetInput } from './InputPanel';
 
 const log = Log.module('@deephaven/js-plugin-module-template.DashboardPlugin');
+
+export const INTERACTIVE_QUERY_OBJECT_TYPE =
+  'deephaven.plugin.interactive.InteractiveQuery';
+
+export function openOutputPanel({
+  panelId = shortid.generate(),
+  fetch,
+  widget,
+  layout,
+}) {
+  log.info('openOutputPanel', panelId, widget);
+
+  const openOptions = { fetch, widget, panelId };
+  layout.eventHub.emit(PanelEvent.OPEN, openOptions);
+}
+
+export function openInputPanel({
+  dragEvent,
+  fetch,
+  layout,
+  localDashboardId = '',
+  panelId = shortid.generate(),
+  widget,
+}: {
+  dragEvent?: DragEvent;
+  fetch: () => Promise<unknown>;
+  layout: GoldenLayout;
+  localDashboardId?: string;
+  panelId?: string;
+  widget: VariableDefinition;
+}) {
+  const { id: widgetId, name, type } = widget;
+
+  // We actually need to open multiple panels for interactive queries. But let's start with the inputs panel
+  const metadata = { id: widgetId, name, type };
+  const config = {
+    type: 'react-component',
+    component: InputPanel.COMPONENT,
+    props: {
+      localDashboardId,
+      id: panelId,
+      metadata,
+      fetch,
+    },
+    title: name,
+    id: panelId,
+  };
+
+  const { root } = layout;
+  LayoutUtils.openComponent({ root, config, dragEvent });
+}
 
 export const DashboardPlugin = ({
   id,
@@ -17,45 +70,25 @@ export const DashboardPlugin = ({
   registerComponent,
 }: DashboardPluginComponentProps): JSX.Element => {
   const handlePanelOpen = useCallback(
-    ({
-      dragEvent,
-      fetch,
-      panelId = shortid.generate(),
-      widget,
-    }: {
+    (props: {
       dragEvent?: DragEvent;
       fetch: () => Promise<unknown>;
       panelId?: string;
       widget: VariableDefinition;
     }) => {
-      const { id: widgetId, name, type } = widget;
-      if (type === dh.VariableType.TABLE || type === dh.VariableType.FIGURE) {
-        // Just ignore table and figure types - only want interesting other types
+      const { id: widgetId, name, type } = props.widget;
+      // Only want to open panels for the interactive query object
+      if ((type as string) == INTERACTIVE_QUERY_OBJECT_TYPE) {
+        log.info('Panel opened of type', type);
+        openInputPanel({ ...props, layout, localDashboardId: id });
         return;
       }
-      log.info('Panel opened of type', type);
-      const metadata = { id: widgetId, name, type };
-      const config = {
-        type: 'react-component',
-        component: ObjectPanel.COMPONENT,
-        props: {
-          localDashboardId: id,
-          id: panelId,
-          metadata,
-          fetch,
-        },
-        title: name,
-        id: panelId,
-      };
-
-      const { root } = layout;
-      LayoutUtils.openComponent({ root, config, dragEvent });
     },
     [id, layout]
   );
 
   useEffect(() => {
-    const cleanups = [registerComponent(ObjectPanel.COMPONENT, ObjectPanel)];
+    const cleanups = [registerComponent(InputPanel.COMPONENT, InputPanel)];
 
     return () => {
       cleanups.forEach(cleanup => cleanup());
